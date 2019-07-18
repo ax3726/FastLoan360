6e7f8257dc90816f109abc4c78ef5cb00557a031
liming <15970060419lma>
2019-07-18 15:21:36 +0800

    修改接口地址

--- app/src/main/java/com/xjd/a360fastloan/common/Link.java
+++ app/src/main/java/com/xjd/a360fastloan/common/Link.java
@@ -5,7 +5,7 @@ package com.xjd.a360fastloan.common;
  */
 
 public class Link {
-    public static final String SEREVE           = "http://cash.free.idcfengye.com/";
+    public static final String SEREVE           = "http://cash.vipgz1.idcfengye.com/";
     public static final String AGREE_REGISTER   = SEREVE + "agreements/?name=用户注册服务协议";
     public static final String AGREE_RECHANGRE  = SEREVE + "agreements/?name=充值服务协议";
     public static final String AGREE_WITH_HOLD  = SEREVE + "agreements/?name=代扣服务协议";
